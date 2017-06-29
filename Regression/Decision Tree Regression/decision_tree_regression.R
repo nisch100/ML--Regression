82e2a4ec4bd4866f094bce5563b4dd626cafe95b
Nischal Reddy <chandra.nischal@gmail.com>
2017-06-29 03:49:35 -0400

    Update decision_tree_regression.R

--- Regression/Decision Tree Regression/decision_tree_regression.R
+++ Regression/Decision Tree Regression/decision_tree_regression.R
@@ -4,19 +4,6 @@
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
-# Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
-
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Fitting Decision Tree Regression to the dataset
 # install.packages('rpart')
 library(rpart)
 regressor = rpart(formula = Salary ~ .,
@@ -26,7 +13,7 @@ regressor = rpart(formula = Salary ~ .,
 # Predicting a new result with Decision Tree Regression
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Decision Tree Regression results (higher resolution)
+# Visualising the Decision Tree with better resolution
 # install.packages('ggplot2')
 library(ggplot2)
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
@@ -39,6 +26,6 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Plotting the tree
+# Plot
 plot(regressor)
 text(regressor)
